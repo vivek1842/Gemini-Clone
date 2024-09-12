5f88dde34c9b7c3f1b4e130c18672093e785cfb5
vivek1842 <vivekghule2002@gmail.com>
2024-09-13 00:50:50 +0530

    gemini api integrated

---
+++ src/config/gemini.js
@@ -0,0 +1,42 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+/*
+ * Install the Generative AI SDK
+ *
+ * $ npm install @google/generative-ai
+ */
+
+import { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } from "@google/generative-ai";
+  
+  const apiKey = process.env.REACT_APP_GEMINI_API_KEY;
+  console.log("apiKey"+apiKey);
+  
+  const genAI = new GoogleGenerativeAI(apiKey);
+  
+  const model = genAI.getGenerativeModel({
+    model: "gemini-1.5-flash",
+  });
+  
+  const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 64,
+    maxOutputTokens: 8192,
+    responseMimeType: "text/plain",
+  };
+  
+  async function run(prompt) {
+    const chatSession = model.startChat({
+      generationConfig,
+      safetySettings,
+   // safetySettings: Adjust safety settings
+   // See https://ai.google.dev/gemini-api/docs/safety-settings
+      history: [
+      ],
+    });
+  
+    const result = await chatSession.sendMessage(prompt);
+    console.log(result.response.text());
+  }
+  
+ export default run;
