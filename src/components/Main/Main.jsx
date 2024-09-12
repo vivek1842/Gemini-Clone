8919a684d9006bd7fb8ecf6f341456688fe8eeb1
vivek1842 <vivekghule2002@gmail.com>
2024-09-12 23:28:18 +0530

    dashboard content added

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -10,6 +10,30 @@ export const Main = () => {
             <p>Gemini</p>
             <img src={assets.user_icon} alt="user icon not found" />
         </div>
+        <div className="main-container">
+            <div className="greet">
+                <p><span>Hello, Vivek</span></p>
+                <p>How can I help you today?</p>
+            </div>
+            <div className="cards">
+                <div className="card">
+                    <p>Best places in japan to visit in spring season</p>
+                    <img src={assets.compass_icon} alt="compass icon not found" />
+                </div>
+                <div className="card">
+                    <p>Explain react hooks with example</p>
+                    <img src={assets.bulb_icon} alt="compass icon not found" />
+                </div>
+                <div className="card">
+                    <p>Suggest best story games to play on latest ps5 pro</p>
+                    <img src={assets.message_icon} alt="compass icon not found" />
+                </div>
+                <div className="card">
+                    <p>Improve performance of the following code</p>
+                    <img src={assets.code_icon} alt="compass icon not found" />
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
