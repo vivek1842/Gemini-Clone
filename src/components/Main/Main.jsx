d95b8ac7e75300ed339f2a365bd2ca12b345392e
vivek1842 <vivekghule2002@gmail.com>
2024-09-13 00:18:19 +0530

    dashboard updated

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -33,6 +33,19 @@ export const Main = () => {
                     <img src={assets.code_icon} alt="compass icon not found" />
                 </div>
             </div>
+            <div className="main-bottom">
+                <div className="search-box">
+                    <input type="text" placeholder='Enter a prompt here'/>
+                    <div>
+                        <img src={assets.gallery_icon} alt="gallery icon not found" />
+                        <img src={assets.mic_icon} alt="mic icon not found" />
+                        <img src={assets.send_icon} alt="send icon not found" />
+                    </div>
+                    <p className="bottom-info">
+                    Gemini may display inaccurate info, including about people, so double-check its responses. <a href="https://support.google.com/gemini/answer/13594961?visit_id=638617636113328241-3788869149&p=privacy_notice&rd=1#privacy_notice">Your privacy and Gemini Apps</a>
+                    </p>
+                </div>
+            </div>
         </div>
     </div>
   )
